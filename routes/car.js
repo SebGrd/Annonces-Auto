5f523d4c63df5e60f0049c5905776cf477c62915
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2020-02-20 23:57:28 +0100

    Recherche par marque ou modele ou les deux

--- routes/car.js
+++ routes/car.js
@@ -11,29 +11,51 @@ car.post('/:id', utils.isAdmin, (req, res) => {
 
 
 car.get('/', (req, res) => {
-    if (req.query.brand){
-        console.log(req.query.brand);
-    } else {
-        console.log('No brand given')
+    if (req.query.brand && !req.query.model){
+        database.carModel.find({brand: req.query.brand}, (err, cars) => {
+            if (err) { //SI ERREUR INTERNE
+                console.log(err);
+                res.status(500)
+                    .json({"error": "Internal server error"});
+            } else if (cars.length){ //SINON SI ELEMENT TROUVED
+                res.status(200);
+                res.send(cars);
+            } else{ //SINON AUCUN ELEMENT TROUVED
+                res.status(404)
+                    .json({ "message" : "No car found for : " + req.query.brand})
+            }
+        });
     }
-    if (req.query.model){
-        console.log(req.query.model);
-    } else {
-        console.log('No model given')
+    if (req.query.model && !req.query.brand){
+        database.carModel.find({model: req.query.model}, (err, cars) => {
+            if (err) { //SI ERREUR INTERNE
+                console.log(err);
+                res.status(500)
+                    .json({"error": "Internal server error"});
+            } else if (cars.length){ //SINON SI ELEMENT TROUVED
+                res.status(200);
+                res.send(cars);
+            } else{ //SINON AUCUN ELEMENT TROUVED
+                res.status(404)
+                    .json({ "message" : "No car found for : " + req.query.model})
+            }
+        });
     }
-    database.carModel.find({brand: req.query.brand}, (err, cars) => {
-        if (err) { //SI ERREUR INTERNE
-            console.log(err);
-            res.status(500)
-                .json({"error": "Internal server error"});
-        } else if (cars.length){ //SINON SI ELEMENT TROUVED
-            res.status(200);
-            res.send(cars);
-        } else{ //SINON AUCUN ELEMENT TROUVED
-            res.status(404)
-                .json({ "message" : "No car found for : " + req.query.brand})
-        }
-    });
+    if (req.query.brand && req.query.model){
+        database.carModel.find({brand: req.query.brand, model: req.query.model}, (err, cars) => {
+            if (err) { //SI ERREUR INTERNE
+                console.log(err);
+                res.status(500)
+                    .json({"error": "Internal server error"});
+            } else if (cars.length){ //SINON SI ELEMENT TROUVED
+                res.status(200);
+                res.send(cars);
+            } else{ //SINON AUCUN ELEMENT TROUVED
+                res.status(404)
+                    .json({ "message" : "No car found for : " + req.query.model})
+            }
+        });
+    }
 });
 
 car.delete('/:id', utils.isAdmin, (req, res) => {
