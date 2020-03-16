f43b44e8111ff2b547f17b671eb52a620e1264b1
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2020-03-16 01:30:23 +0100

    Route single annonce

--- routes/views.js
+++ routes/views.js
@@ -13,4 +13,10 @@ views.get('/liste-annonces', (req, res, next) => {
     res.render('liste-annonces', {title: 'Liste des annonces'});
 });
 
+//Single annonce
+views.get('/liste-annonces/:id', (req, res, next) => {
+    res.status(200);
+    res.render('single-annonce', {title: 'Liste des annonces'});
+});
+
 module.exports = views;
